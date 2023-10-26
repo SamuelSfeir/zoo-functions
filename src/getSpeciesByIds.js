61671aef061017b45ef8b6255fc5172e73e63aff
Samuel Sfeir <samuel1808@hotmail.com>
2023-10-26 10:37:57 -0300

    Adiciona requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
 
-const getSpeciesByIds = (ids) => {
-  // seu código aquii
+const getSpeciesByIds = (...ids) => {
+  if (ids.length === 0) {
+    return [];
+  } {
+    const speciesFound = data.species.filter((species) => ids.includes(species.id));
+    return speciesFound;
+  }
 };
+console.log(getSpeciesByIds('ef3778eb-2844-4c7c-b66c-f432073e1c6b'));
 
 module.exports = getSpeciesByIds;
