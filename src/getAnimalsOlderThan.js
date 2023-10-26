3a0af163fc43a7d2eb5d27c8c51fa3edc0ddbaad
Samuel Sfeir <samuel1808@hotmail.com>
2023-10-26 11:37:55 -0300

    Adiciona requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
 
 const getAnimalsOlderThan = (animal, age) => {
-  // seu código aqui
+  const searchName = data.species.find((name) => name.name === animal);
+  const searchAge = searchName.residents.find((ageofAnimal) => ageofAnimal.age === age);
+  if (searchAge) { // Doesn't exist
+    return false;
+  }
+  return true; // exists
 };
 
 module.exports = getAnimalsOlderThan;
