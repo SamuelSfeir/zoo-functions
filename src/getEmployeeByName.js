ecb0f85f0d6706e3bc51ae24462c08e653046716
Samuel Sfeir <samuel1808@hotmail.com>
2023-10-27 10:01:11 -0300

    Adiciona requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,16 @@
 const data = require('../data/zoo_data');
 
 const getEmployeeByName = (employeeName) => {
-  // seu código aqui
-};
+  if (!employeeName) {
+    return {};
+  }
 
+  const employee = data.employees.find((findEmployee) => {
+    const { firstName, lastName } = findEmployee;
+    return employeeName === firstName || employeeName === lastName;
+  });
+
+  return employee || {};
+};
+// console.log(getEmployeeByName('Nigel'));
 module.exports = getEmployeeByName;
