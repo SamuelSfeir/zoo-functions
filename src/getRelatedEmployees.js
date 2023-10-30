fc76a9eaaf9e85823df231cde751199aeed76a07
Samuel Sfeir <samuel1808@hotmail.com>
2023-10-30 08:51:51 -0300

    Adiciona requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,25 @@
 const data = require('../data/zoo_data');
 
 const isManager = (id) => {
-  // seu código aqui
+  const isManager1 = data.employees.some((employee) => employee.managers.includes(id));
+  return isManager1;
 };
 
 const getRelatedEmployees = (managerId) => {
-  // seu código aqui
+  if (!isManager(managerId)) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+
+  const { employees } = data;
+
+  const findManagedEmployees = (person) => person.managers.includes(managerId);
+  const formatFullName = (person) => `${person.firstName} ${person.lastName}`;
+
+  const relatedEmployees = employees
+    .filter(findManagedEmployees)
+    .map(formatFullName);
+
+  return relatedEmployees;
 };
 
 module.exports = { isManager, getRelatedEmployees };
