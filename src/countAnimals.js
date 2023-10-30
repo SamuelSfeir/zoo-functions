e17e5103152f14e7d58abe7587c868068aab3c3a
Samuel Sfeir <samuel1808@hotmail.com>
2023-10-30 09:41:33 -0300

    Adiciona requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,25 @@
 const data = require('../data/zoo_data');
 
 const countAnimals = (animal) => {
-  // seu código aqui
+  const { species } = data;
+
+  if (!animal) {
+    const animalCount = species.reduce((acc, specie) => ({
+      ...acc,
+      [specie.name]: specie.residents.length,
+    }), {});
+    return animalCount;
+  }
+
+  const matchingSpecies = species.find((specie) => specie.name === animal.species);
+
+  if (!animal.sex) {
+    return matchingSpecies.residents.length;
+  }
+
+  const matchingSexCount = matchingSpecies.residents.filter((resident) =>
+    resident.sex === animal.sex).length;
+  return matchingSexCount;
 };
 
 module.exports = countAnimals;
