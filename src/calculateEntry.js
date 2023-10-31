fd5b0bbb1820672cc6402f35205705bb1952101e
Samuel Sfeir <samuel1808@hotmail.com>
2023-10-31 09:23:48 -0300

    Adiciona requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,32 @@
 const data = require('../data/zoo_data');
 
 const countEntrants = (entrants) => {
-  // seu código aqui
+  const counts = { child: 0, adult: 0, senior: 0 };
+
+  entrants.forEach((entrant) => {
+    if (entrant.age < 18) {
+      counts.child += 1;
+    } else if (entrant.age < 50) {
+      counts.adult += 1;
+    } else {
+      counts.senior += 1;
+    }
+  });
+
+  return counts;
 };
 
 const calculateEntry = (entrants) => {
-  // seu código aqui
+  if (!entrants || entrants.length === 0) {
+    return 0;
+  }
+  const { child, adult, senior } = data.prices;
+  const inputEntrants = Object.values(countEntrants(entrants));
+  let somaEntrants = 0;
+  somaEntrants += inputEntrants[0] * child;
+  somaEntrants += inputEntrants[1] * adult;
+  somaEntrants += inputEntrants[2] * senior;
+  return somaEntrants;
 };
 
 module.exports = { calculateEntry, countEntrants };
