4efb81a12fa943aa041c7b520204d83e491ae70b
Samuel Sfeir <samuel1808@hotmail.com>
2023-11-01 10:58:57 -0300

    Adiciona requisito 11

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,64 @@
 const data = require('../data/zoo_data');
 
-const getEmployeesCoverage = () => {
-  // seu código aqui
-};
+const { species } = data;
+const { employees } = data;
+
+function getSpeciesResponsibleFor(employeeId) {
+  const foundEmp = employees.find((element) => element.id === employeeId);
+  const arrayOfSpecies = foundEmp.responsibleFor;
+  const arrayObjectsSpecies = [];
+  arrayOfSpecies.forEach((element1) => {
+    arrayObjectsSpecies.push(species.find((element2) => element2.id === element1));
+  });
+  const arraySpeciesNames = arrayObjectsSpecies.map((element) => element.name);
+  return arraySpeciesNames;
+}
+
+function getAreas(employeeId) {
+  const foundEmp = employees.find((element) => element.id === employeeId);
+  const arraySpecies = foundEmp.responsibleFor;
+  const arrayObjectsSpecies = [];
+  arraySpecies.forEach((element1) => {
+    arrayObjectsSpecies.push(species.find((element2) => element2.id === element1));
+  });
+  const arrayAreas = arrayObjectsSpecies.map((element) => element.location);
+  return arrayAreas;
+}
+
+function generateAllEmployessCoverage() {
+  const returnedArray = [];
+  employees.forEach((element) => {
+    const employeeObject = {
+      id: element.id,
+      fullName: `${element.firstName} ${element.lastName}`,
+      species: getSpeciesResponsibleFor(element.id),
+      locations: getAreas(element.id),
+    };
+    returnedArray.push(employeeObject);
+  });
+  return returnedArray;
+}
+
+function verifyParameter(parameter) {
+  const isId = employees.some((element) => element.id === parameter.id);
+  const isItFirstName = employees.some((element) => element.firstName === parameter.name);
+  const isItLastName = employees.some((element) => element.lastName === parameter.name);
+  if (isId) return 'itsId';
+  if (isItFirstName || isItLastName) return 'itsName';
+  return false;
+}
+
+function getEmployeesCoverage(object) {
+  if (!object) return generateAllEmployessCoverage();
+  const parameterType = verifyParameter(object);
+  if (!parameterType) throw new Error('Informações inválidas');
+  if (parameterType === 'itsId') {
+    return generateAllEmployessCoverage().find((element) => element.id === object.id);
+  } if (parameterType === 'itsName') {
+    const employee = employees
+      .find((element) => element.firstName === object.name || element.lastName === object.name);
+    return generateAllEmployessCoverage().find((element) => element.id === employee.id);
+  }
+}
 
 module.exports = getEmployeesCoverage;
