431f30a2d68a9d6437248f37193b3328b6bd20ef
Samuel Sfeir <samuel1808@hotmail.com>
2023-11-01 10:14:46 -0300

    Adiciona requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,15 @@
 const data = require('../data/zoo_data');
 
+const { employees, species: animalSpecies } = data;
+
 const getOldestFromFirstSpecies = (id) => {
-  // seu código aqui
+  const employeeInfo = employees.find((emp) => emp.id === id);
+  const firstSpeciesId = employeeInfo.responsibleFor[0];
+  const speciesInfo = animalSpecies.find((specie) => specie.id === firstSpeciesId);
+  const sortedAges = speciesInfo.residents
+    .sort((a, b) => b.age - a.age);
+  const oldestResident = sortedAges[0];
+  return [oldestResident.name, oldestResident.sex, oldestResident.age];
 };
 
 module.exports = getOldestFromFirstSpecies;
