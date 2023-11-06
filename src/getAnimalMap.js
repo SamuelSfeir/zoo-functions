4f089efedda8ee0d7db13ed6251f9f9fd8f24db6
Samuel Sfeir <samuel1808@hotmail.com>
2023-11-06 09:28:24 -0300

    Adiciona requisito 14 - duas primeiras funcoes

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,5 +1,31 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
+// PT-BR: Função que recebe uma região como parâmetro e retorna um array com as espécies dessa região
+// EN: Function that takes a region as a parameter and returns an array with the species in that region
+const getSpeciesFromLocation = (location) => {
+  const arrayWithAnimalsName = [];
+  const animals = species.filter((element) => element.location === location);
+  animals.forEach((element) => {
+    arrayWithAnimalsName.push(element.name);
+  });
+  return arrayWithAnimalsName;
+};
+
+// PT-BR: Função que verifica se o parâmetro da função principal é 'includeNames: true'
+// EN: Function that checks if the main function parameter is 'includeNames: true'
+const verifyIncludeNames = (parameter) => {
+  const arrayKeys = Object.keys(parameter);
+  const arrayWithNames = [];
+  arrayKeys.forEach((element) => {
+    if (element === 'includeNames') {
+      arrayWithNames.push(element);
+    }
+  });
+  return arrayWithNames.length !== 0;
+};
+
 const getAnimalMap = (options) => {
   // seu código aqui
 };
